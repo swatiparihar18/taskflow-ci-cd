41902f864e165632fe90786f5559fbdbde75f2a1
Swati Parihar <swatiparihar577@gmail.com>
2026-06-08 13:12:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-app.use(express.json());
-
 app.get("/", (req, res) => {
-  res.send("TaskFlow Running 🚀");
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(5000, "0.0.0.0", () => {
